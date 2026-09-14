9d031d3a424c10eb18510c971a16622c402b9c91
mattr13000 <mattjeux13@gmail.com>
2026-09-14 14:27:04 +0200

    Initial Commit

---
+++ FestiWind/FestiWindApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestiWindApp.swift
+//  FestiWind
+//
+//  Created by Apprenant 77 on 14/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct FestiWindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
