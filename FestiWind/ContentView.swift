9f27937bb385c93734543d816e4fdcfb5bad4dc6
mattr13000 <mattjeux13@gmail.com>
2026-09-14 16:33:26 +0200

    added custom colors, custom font, home background imageset

--- FestiWind/ContentView.swift
+++ FestiWind/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.custom("Shalimar-Regular", size: 24))
         }
         .padding()
     }
