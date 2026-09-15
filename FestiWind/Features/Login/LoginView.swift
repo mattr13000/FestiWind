78c980aaa71837b97a94ee417686c58634d00505
mattr13000 <mattjeux13@gmail.com>
2026-09-15 14:29:10 +0200

    register view done, navigation next

--- FestiWind/Features/Login/LoginView.swift
+++ FestiWind/Features/Login/LoginView.swift
@@ -15,20 +15,14 @@ struct LoginView: View {
     var body: some View {
         ZStack {
                     HomeBackgroundImageView()
-            
                     VStack {
                         Spacer()
                         Text("Festi Wind")
                             .font(.custom("Shalimar-Regular", size: 80))
                             .padding(.bottom, 60)
                         VStack (spacing: 16){
-                            TextFieldView(textFieldContent: $email, frameMaxWidth: 330)
-                            SecureField("Password", text: $password)
-                                .padding()
-                                .background(.white)
-                                .cornerRadius(5)
-                                .frame(maxWidth: 330)
-                                .shadow(color: .textSecondary, radius: 1, x: 0, y: 1)
+                            TextFieldView(textFieldContent: $email, placeholderText: "Email", frameMaxWidth: 330, isSecure: false)
+                            TextFieldView(textFieldContent: $password, placeholderText: "Password", frameMaxWidth: 330, isSecure: true)
                             HStack {
                                 TextButtonView(buttonText: "Forgot Password ?")
                             }
--- FestiWind/Features/Register/RegisterView.swift
+++ FestiWind/Features/Register/RegisterView.swift
@@ -11,29 +11,36 @@ struct RegisterView: View {
     @State private var name: String = ""
     var body: some View {
         NavigationStack {
-            VStack {
-                Spacer()
-                VStack(spacing: 20) {
-                    HStack {
-                        TextFieldView(textFieldContent: $name, frameMaxWidth: 150)
-                        Spacer()
-                        TextFieldView(textFieldContent: $name, frameMaxWidth: 150)
+        ZStack {
+            Color.backgroundBlue
+                .ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    VStack(spacing: 20) {
+                        HStack {
+                            TextFieldView(textFieldContent: $name, placeholderText: "Name", frameMaxWidth: 150, isSecure: false)
+                            Spacer()
+                            TextFieldView(textFieldContent: $name, placeholderText: "Surname", frameMaxWidth: 150, isSecure: false)
+                        }
+                        .frame(maxWidth: 330)
+                        TextFieldView(textFieldContent: $name, placeholderText: "Email", frameMaxWidth: 330, isSecure: false)
+                        TextFieldView(textFieldContent: $name, placeholderText: "Password", frameMaxWidth: 330, isSecure: true)
+                        TextFieldView(textFieldContent: $name, placeholderText: "Confirm Password", frameMaxWidth: 330, isSecure: true)
                     }
-                    .frame(maxWidth: 330)
-                    TextFieldView(textFieldContent: $name, frameMaxWidth: 330)
-                    TextFieldView(textFieldContent: $name, frameMaxWidth: 330)
-                    TextFieldView(textFieldContent: $name, frameMaxWidth: 330)
+                    Spacer()
+                    GlassButtonView(buttonText: "Register", frameMaxWidth: 280, opacity: 0.3, textColor: .textPrimary, backgroundColor: .buttonColorActive)
+                    HStack {
+                        Text("Already have an account ?")
+                        TextButtonView(buttonText: "Log in")
+                    }
+                    Spacer()
                 }
-                Spacer()
-                GlassButtonView(buttonText: "Register", frameMaxWidth: 280, opacity: 0.3, textColor: .textPrimary, backgroundColor: .buttonColorActive)
-                HStack {
-                    Text("Already have an account ?")
-                    TextButtonView(buttonText: "Log in")
-                }
-                Spacer()
+                .navigationTitle("Register")
+                
             }
-            .navigationTitle("Register")
+            
         }
+        
     }
 }
 
