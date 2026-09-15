fc288fef7a17a38a321e57fbaf2965831adc6b28
mattr13000 <mattjeux13@gmail.com>
2026-09-15 15:02:40 +0200

    schedule card done

---
+++ FestiWind/Features/Schedule/Components/ScheduleCard.swift
@@ -0,0 +1,51 @@
+//
+//  ScheduleCard.swift
+//  FestiWind
+//
+//  Created by Apprenant 77 on 15/09/2026.
+//
+
+import SwiftUI
+
+struct ScheduleCard: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Kite surf")
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+            
+            HStack() {
+                Image(systemName: "clock")
+                    .font(.subheadline)
+                Text("11 pm to 12 pm")
+                    .font(.subheadline)
+            }
+            .foregroundColor(.black)
+            
+            HStack {
+                Spacer()
+                Text("Remaining places: 25")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            GlassButtonView(
+                buttonText: "Book now",
+                frameMaxWidth: .infinity,
+                frameMaxHeight: 1,
+                opacity: 0.3,
+                textColor: .textPrimary,
+                backgroundColor: .buttonColorActive
+            )
+            .padding(.bottom, 10)
+        }
+        .padding(30)
+        .background(
+            CardTemplateView()
+        )
+    }
+}
+
+#Preview {
+    ScheduleCard()
+}
---
+++ FestiWind/Features/Schedule/ScheduleView.swift
@@ -0,0 +1,29 @@
+//
+//  ScheduleView.swift
+//  FestiWind
+//
+//  Created by Apprenant 77 on 15/09/2026.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color.backgroundBlue
+                    .ignoresSafeArea()
+                ScrollView {
+                    VStack {
+                        
+                    }
+                }
+                .navigationTitle("Schedule")
+            }
+        }
+    }
+}
+
+#Preview {
+    ScheduleView()
+}
