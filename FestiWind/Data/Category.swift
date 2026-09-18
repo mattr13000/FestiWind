d70e4e7dcd92f3929b791564de52eaccda67850d
Moutiez <ymoutiez@outlook.fr>
2026-09-18 10:51:16 +0200

    add Data

---
+++ FestiWind/Data/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  FestiWind
+//
+//  Created by Apprenant 87 on 18/09/2026.
+//
+
+import Foundation
+
+struct Category{
+    let id: Int
+    let name: String
+}
---
+++ FestiWind/Features/Schedule/ScheduleViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  ScheduleViewModel.swift
+//  FestiWind
+//
+//  Created by Apprenant 87 on 18/09/2026.
+//
+
